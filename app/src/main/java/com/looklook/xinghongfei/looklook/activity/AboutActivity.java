3c734a5715979cab1d71152fe322ca5171c55515
top2015 <haitao_2015@163.com>
2017-06-03 11:37:54 +0800

    修改了关于界面

--- app/src/main/java/com/looklook/xinghongfei/looklook/activity/AboutActivity.java
+++ app/src/main/java/com/looklook/xinghongfei/looklook/activity/AboutActivity.java
@@ -2,15 +2,30 @@ package com.looklook.xinghongfei.looklook.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.LinearLayout;
+
 import com.looklook.xinghongfei.looklook.R;
 
 /**
- * Created by xinghongfei on 16/8/25.
+ * Created by top2015 on 17/6/3.
  */
-public class AboutActivity extends BaseActivity {
+
+
+public class AboutActivity extends BaseActivity implements View.OnClickListener{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        LinearLayout draggable_frame = (LinearLayout) findViewById(R.id.draggable_frame);
+        draggable_frame.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+    finish();
+
+    }
+
+
 }
